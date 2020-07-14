bab3f0fb4712821dc89ba77b5a2f06746dfba2b5
Jai Sharma <sendtojsharma@gmail.com>
2020-07-14 08:37:51 +0530

    added

--- js/index.js
+++ js/index.js
@@ -1,21 +1,21 @@
-import Search from './models/Search';
-import * as searchView from './views/searchView';
-import { elements, renderLoader, clearLoader } from './views/base';
-
+import Search from "./models/Search";
+import Recipe from "./models/Recipe";
+import * as searchView from "./views/searchView";
+import { elements, renderLoader, clearLoader } from "./views/base";
 
 /* Global state of the app
  * - Search Object
  * - Current recipe object
- * - Shopping list object 
+ * - Shopping list object
  * - Liked recipes
-*/
-const state = {}
+ */
+const state = {};
 
+// search
 const controlSearch = async () => {
-
     const query = searchView.getInput(); // TODO
 
-    if(query){
+    if (query) {
         state.search = new Search(query);
 
         // prepare UI
@@ -23,16 +23,55 @@ const controlSearch = async () => {
         searchView.clearResults();
         renderLoader(elements.searchRes);
 
-        await state.search.getResults();
+        try {
+            await state.search.getResults();
 
-        // render results
-        clearLoader();
-        searchView.renderResults(state.search.results);
+            // render results
+            clearLoader();
+            searchView.renderResults(state.search.results);
+        } catch (err) {
+            clearLoader();
+            console.log(err);
+        }
     }
+};
 
-}
-
-elements.searchForm.addEventListener('submit', e => {
+elements.searchForm.addEventListener("submit", (e) => {
     e.preventDefault();
     controlSearch();
 });
+
+elements.searchResPages.addEventListener("click", (e) => {
+    const btn = e.target.closest(".btn-inline");
+    if (btn) {
+        const goToPage = +btn.dataset.goto;
+        searchView.clearResults();
+        searchView.renderResults(state.search.results, goToPage);
+    }
+});
+
+// recipe
+const controlRecipe = async () => {
+    const id = window.location.hash.replace("#", "");
+
+    if (id) {
+        // prepare UI
+
+        // create recipe
+        state.recipe = new Recipe(id);
+
+        try {
+            // get recipe data
+            await state.recipe.getRecipe();
+
+            // calculate servings and time
+            state.recipe.calcTime();
+            state.recipe.calcServings();
+        } catch (err) {
+            console.log(err);
+        }
+    }
+};
+
+// adding to both the events
+["hashchange", "load"].forEach((event) => window.addEventListener(event, controlRecipe));
